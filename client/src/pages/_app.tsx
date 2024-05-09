cbbfe10eb23a344081148b8aaabfffcabce82894
vyask21 <vyasks01@gmail.com>
2024-05-09 18:33:08 -0500

    commit

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "react-query";
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 const queryClient = new QueryClient();
